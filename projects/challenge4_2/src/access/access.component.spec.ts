c355355d02da0bfa108eaf909527daf97d8de9d9
raulgonzalez <raulgonji@gmail.com>
2024-03-20 11:42:18 +0100

    Update all test

--- projects/challenge4_2/src/access/access.component.spec.ts
+++ projects/challenge4_2/src/access/access.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AccessComponent } from './access.component';
+import AccessComponent from './access.component';
 
 describe('AccesComponent', () => {
   let component: AccessComponent;
